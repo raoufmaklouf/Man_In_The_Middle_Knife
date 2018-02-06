c178024195cbd3f52cb75d365118a2ef168bcc2a
raoufmaklouf <raoufmaklouf58@gmail.com>
2018-02-06 13:07:44 +0000

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install scapy
+sudo apt-get install pip
+sudo pip install subprocess
+sudo pip install threading
+sudo pip install signal
+sudo apt-get install wireshark
+sudo apt-get install nmap 
